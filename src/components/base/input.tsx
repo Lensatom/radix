a27abdc519fbf5ec87f6bd4f27175eee5072aabc
Lensatom <ayomidetommiwa@gmail.com>
2024-04-05 19:17:41 +0100

    Type fixing

--- src/components/base/input.tsx
+++ src/components/base/input.tsx
@@ -18,7 +18,6 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
               "flex h-24 resize-none w-full rounded-md border-[1px] border-gray px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-xs focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
               className
             )}
-            {...props}
           ></textarea>
         ) : (
           <input
